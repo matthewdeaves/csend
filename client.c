a38fd048e1ab802da0b1d35fdb1f347b9163cc18
Matthew Deaves <mndeaves@me.com>
2025-03-16 18:00:46 +0000

    Very simple client server communication

---
+++ client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+    
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+    
+    // Send message to server
+    char *message = "Hello from client";
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to server\n");
+    
+    // Receive response from server
+    read(sock, buffer, BUFFER_SIZE);
+    printf("Response from server: %s\n", buffer);
+    
+    // Close the socket
+    close(sock);
+    
+    return 0;
+}
